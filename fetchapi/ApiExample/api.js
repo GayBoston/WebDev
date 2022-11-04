5984d0d7ddef67d78bccc62cea1c3516b7b72b53
Gabe <austing@mssm.org>
2022-11-04 07:25:58 -0600

    BALLS

--- fetchapi/ApiExample/api.js
+++ fetchapi/ApiExample/api.js
@@ -34,6 +34,52 @@ function htmlEnd(res)
 
 
 
+let players = [
+            {name: "Bogdan Bogdanovic", team: "Hawks", age: 30, height: 78, link: `https://www.basketball-reference.com/players/b/bogdanbo01.html`},
+            {name: "Clint Capela", team: "Hawks", age: 28, height: 82, link: `https://www.basketball-reference.com/players/c/capelcl01.html`},
+            {name: "John Collins", team: "Hawks", age: 25, height: 81, link: `https://www.basketball-reference.com/players/c/collijo01.html`},
+            {name: "Jarett Culver", team: "Hawks", age: 23, height: 78, link: `https://www.basketball-reference.com/players/c/culveja01.html`},
+            {name: "AJ Griffin", team: "Hawks", age: 19, height: 78, link: `https://www.basketball-reference.com/players/g/griffaj01.html`},
+            {name: "Aaron Holiday", team: "Hawks", age: 26, height: 72, link: `https://www.basketball-reference.com/players/h/holidaa01.html`},
+            {name: "De'andre Hunter", team: "Hawks", age: 24, height: 80, link: `https://www.basketball-reference.com/players/h/huntede01.html`},
+            {name: "Dejounte Murray", team: "Hawks", age: 26, height: 76, link: `https://www.basketball-reference.com/players/m/murrade01.html`},
+            {name: "Trae Young", team: "Hawks", age: 24, height: 73, link: `https://www.basketball-reference.com/players/y/youngtr01.html`},
+            {name: "Onyeka Okongwu", team: "Hawks", age: 21, height: 80, link: `https://www.basketball-reference.com/players/o/okongon01.html`},
+            
+            {name: "Jayson Tatum", team: "Celtics", age: 24, height: 80, link: `https://www.basketball-reference.com/players/t/tatumja01.html`},
+            {name: "Jaylen Brown", team: "Celtics", age: 26, height: 78, link: `https://www.basketball-reference.com/players/b/brownja02.html`},
+            {name: "Marcus Smart", team: "Celtics", age: 28, height: 76, link: `https://www.basketball-reference.com/players/s/smartma01.html`},
+            {name: "Malcolm Brogdon", team: "Celtics", age: 29, height: 76, link: `https://www.basketball-reference.com/players/b/brogdma01.html`},
+            {name: "Al Horford", team: "Celtics", age: 36, height: 81, link: `https://www.basketball-reference.com/players/h/horfoal01.html`},
+            {name: "Robert Williams", team: "Celtics", age: 25, height: 81, link: `https://www.basketball-reference.com/players/w/williro04.html`},
+            {name: "Derrick White", team: "Celtics", age: 28, height: 76, link: `https://www.basketball-reference.com/players/w/whitede01.html`},
+            {name: "Noah Vonleh", team: "Celtics", age: 27, height: 82, link: `https://www.basketball-reference.com/players/v/vonleno01.html`},
+            {name: "JD Davidson", team: "Celtics", age: 20, height: 73, link: `https://www.basketball-reference.com/players/d/davidjd01.html`},
+            {name: "Danilo Gallinari", team: "Celtics", age: 34, height: 82, link: `https://www.basketball-reference.com/players/g/gallida01.html`},
+            {name: "Blake Griffin", team: "Celtics", age: 33, height: 81, link: `https://www.basketball-reference.com/players/g/griffbl01.html`},
+            {name: "Justin Jackson", team: "Celtics", age: 27, height: 80, link: `https://www.basketball-reference.com/players/j/jacksju01.html`},
+            {name: "Mfiondu Kabengele", team: "Celtics", age: 25, height: 82, link: `https://www.basketball-reference.com/players/k/kabenmf01.html`},
+            {name: "Luke Kornet", team: "Celtics", age: 27, height: 86, link: `https://www.basketball-reference.com/players/k/kornelu01.html`},
+            {name: "Payton Pritchard", team: "Celtics", age: 24, height: 73, link: `https://www.basketball-reference.com/players/p/pritchpa01.html`},
+            {name: "Grant Williams", team: "Celtics", age: 23, height: 78, link: `https://www.basketball-reference.com/players/w/willigr01.html`}];
+
+
+            
+app.get('/', (req,res) => {
+    res.json(players);
+})
+
+
+
+app.get('/players', (req,res) => {
+    res.json(players);
+})
+
+app.get('/players/:playerID', (req,res) => {
+    res.json(players[req.params.playerID]);
+})
+
+
 
 
 app.listen(port, () => {
